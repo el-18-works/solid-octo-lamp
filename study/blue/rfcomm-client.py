7cd05b92c4d1a07ffa42bc0978536a942dc082d4
Luckxa <satlin_luckxa@yahoo.com>
2019-04-06 10:02:33 +0900

    recieve a file from air to bodhi, reciever.

--- study/blue/rfcomm-client.py
+++ study/blue/rfcomm-client.py
@@ -2,6 +2,7 @@
 from bluetooth import bluez
 
 my_addr ="48:D7:05:DF:C6:4C"
+my_addr ="00:1B:DC:05:B8:AF"
 my_port =1
 
 sock =bluez.BluetoothSocket( bluez.RFCOMM )
@@ -9,3 +10,4 @@ sock.connect((my_addr, my_port))
 
 sock.send("hello!")
 sock.close()
+
