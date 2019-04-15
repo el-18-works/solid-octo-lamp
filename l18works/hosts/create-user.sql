f6f9d24a93360be1744151c5d9458bdc2385fc7d
Luckxa <satlin_luckxa@yahoo.com>
2019-04-16 05:12:51 +0900

    inicializacion de php

--- l18works/hosts/create-user.sql
+++ l18works/hosts/create-user.sql
@@ -1,16 +1,22 @@
 
-CREATE USER IF NOT EXISTS 'luckxa'@'localhost'
-	IDENTIFIED BY 'mylinka';
-GRANT ALL
-	ON *.*
-	TO 'luckxa'@'localhost';
+-- DROP USER IF EXISTS 'l18'@'localhost';
+-- DROP USER IF EXISTS 'luckxa'@'localhost';
 
 CREATE USER IF NOT EXISTS 'l18'@'localhost'
-	IDENTIFIED BY '';
-GRANT SELECT
-	ON *.*
+	IDENTIFIED BY '@';
+GRANT ALL ON *.*
 	TO 'l18'@'localhost';
-GRANT ALL
-	ON l18.*
+-- PARA CREATE PROCEDURE/FUNCTION :
+GRANT SUPER ON *.*
 	TO 'l18'@'localhost';
+
+CREATE USER IF NOT EXISTS 
+	'luckxa'@'localhost' 
+	IDENTIFIED BY 'MyLinka';
+GRANT SELECT ON *.*
+	TO 'luckxa'@'localhost';
+GRANT ALL ON l18.*
+	TO 'luckxa'@'localhost';
+GRANT ALL ON luckxa.*
+	TO 'luckxa'@'localhost';
 
