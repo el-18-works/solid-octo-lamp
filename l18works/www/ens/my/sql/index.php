216073def483ef0682c08aaaf46da58fa6ced649
Luckxa <satlin_luckxa@yahoo.com>
2019-04-15 19:42:55 +0900

    mysql côté serveur.

--- l18works/www/ens/my/sql/index.php
+++ l18works/www/ens/my/sql/index.php
@@ -12,44 +12,16 @@ $q = json_decode($_REQUEST["q"], false /*=> object, true => array*/);
 $p = isset($_REQUEST["p"]) ? json_decode($_REQUEST["p"], false /*=> object, true => array*/) : null;
 // argumento de conexión
 $c = json_decode($_REQUEST["c"], false /*=> object, true => array*/);
-setcookie("c", $_REQUEST["c"], time()+60*60*24*90); /*90 días*/
-
-function my_describe() {
-	if (!($stmt = $mysqli->prepare($q))) {
-	//if (!($stmt = $mysqli->prepare("DESCRIBE $table"))) {
-		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
-	}
-	if (!$stmt->execute()) {
-		echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
-	}
-	if (!$stmt->store_result()) {
-		echo "Store failed: (" . $mysqli->errno . ") " . $mysqli->error;
-	}
-	//printf("Num rows = %d \n", $stmt->num_rows());
-	//printf("Field count = %d \n", $stmt->field_count);
-	$out =array();
-	if (!$stmt->bind_result($out["Field"], $out["Type"], $out["Null"], $out["Key"], $out["Default"], $out["Extra"])) {
-		echo "Binding output parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-	}
-	echo "<p class='stmt'><span class='prompt'>mysql&gt; DESCRIBE</span>&nbsp; $table <span class='prompt'>;</span><p>";
-	echo "<table class='desc'>";
-	echo "<tr><th>Field</th><th>Type</th><th>Null</th><th>Key</th><th>Default</th><th>Extra</th></tr>";
-	while ($stmt->fetch()) {
-		echo "<tr>";
-		foreach( $out as $value ) {
-			printf("<td>%s</td>", $value);
-		}
-		echo "</tr>";
-	}
-	echo "</table>";
-}
+// XXX cookie no efectuante.
+//setcookie("c", $_REQUEST["c"], time()+60*60*24*90); /*90 días*/
 
 class resclass {
 	var $error;
 	var $res;
 }
 
-$mysqli =new mysqli("localhost", "luckxa", "my", "menagerie");
+$mysqli =new mysqli("localhost", $c->user, $c->pass, $c->schema);
+//$mysqli =new mysqli("localhost", "luckxa", "my", "sys");
 $r =new resclass();
 if ($p != null) { // preparando con parámetros.
 	$fmt =$p[0];
