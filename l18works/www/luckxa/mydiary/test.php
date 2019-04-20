123029395518171624d1552d83c1454de8d6e57b
Luckxa <satlin_luckxa@yahoo.com>
2019-04-21 02:57:35 +0900

    iniciando mydiary

--- l18works/www/luckxa/mydiary/test.php
+++ l18works/www/luckxa/mydiary/test.php
@@ -37,10 +37,6 @@ if ($MOBILE) {
 	echo "padding : 50px;\n";
 }
 ?>
-/*
-	margin : 50px;
-	padding : 50px;
-*/
 	background-color : white;
 	font-family : 'Alice', "Yu Gothic", "Hiragino Kaku Gothic ProN", "メイリオ", "Ume Gothic S4", sans-serif;
 	line-height : 40px;
@@ -112,8 +108,8 @@ a:hover {
 	border-radius : 50%;
 	float : right;
 	position :relative;
-	top : 23px;
-	right : 85px;
+	top : 13px;
+	right : 125px;
 	opacity : 0.39;
 	z-index : 2;
 }
@@ -129,48 +125,32 @@ a:hover {
 </style>
 
 <script>
-function my_nowloading_init(dur =1) {
-    let e =document.createElement("div");
-    e.id ="nowloading";
-    e.style.position ="fixed";
-    e.style.height ="100%";
-    e.style.width ="100%";
-    e.style.backgroundColor ="#000";
-    e.style.backgroundImage ="url(https://ens.l18.work/res/misc/nowloading.gif)";
-    e.style.backgroundRepeat ="no-repeat";
-    e.style.backgroundPosition ="center center";
-    e.style.backgroundAttachment ="fixed";
-    e.style.opacity ="0.8";
-    e.style.transitionDuration ="1s";
-    document.body.insertBefore(e, document.body.firstChild);
-}
-function my_nowloading_fini(dur =1) {
-    let e =document.getElementById("nowloading");
-    e.style.opacity ="0";
-    setTimeout( () => (document.body.removeChild(e)) , dur * 1000);
-}
+/*
+*/
+<?php my_nowloading(1); ?>
 </script>
 </head> <body>
 
 <script>
-my_nowloading_init();
+myNowloadingInit();
 </script>
 <a id=startdash href="//luckxa.l18.work/start-dash-diary/#bot">麻衣ねぇがリーダー就任するまでの日記</a>
 
 <div id=mypagetitle>
 <img src=xxxtmpdata/miku_400x400.jpg height=40px id=mikuicon class="titleicon"> 
-<img src=xxxtmpdata/sakura.svg height=40px id=sakuraicon class="titleicon"> 
-<img src=xxxtmpdata/sakura.svg height=40px id=sakuraicon1 class="titleicon"> 
+<img src=xxxtmpdata/stamp.png height=80px id=sakuraicon1 class="titleicon"> 
 <h1 class="titleicon">my<ruby>流華<rt>るか</rt></ruby>日記</h1>
 <!--
+<img src=xxxtmpdata/sakura.svg height=40px id=sakuraicon class="titleicon"> 
+<img src=xxxtmpdata/sakura.svg height=40px id=sakuraicon1 class="titleicon"> 
 -->
 <svg id=jumpsvg class=titleicon width=180px height=80px>
  <polygon points="50,3 40,20  60,20" style="fill:lightblue" />
  <rect width=180px height=60px x=0 y=20 rx=20 ry=20 style="fill:lightblue" />
- <a xlink:href="#bot" target="_self">
+ <a xlink:href="https://ja.wikipedia.org/wiki/%E9%83%B5%E6%94%BF%E8%A8%98%E5%BF%B5%E6%97%A5" target="_self">
 <text style="font-family:gothic;font-size:20px;text-shadow:3px 3px 4px grey;fill:#fee" transform="rotate(-12 20,40)">
-  <tspan x=5px y=55px>一番下へ</tspan>
-  <tspan x=70px y=80px>ジャンプw</tspan>
+  <tspan x=5px y=55px>今日は</tspan>
+  <tspan x=70px y=80px>郵政記念日</tspan>
   </text>
  </a>
 </svg>
@@ -178,60 +158,117 @@ my_nowloading_init();
 <div style="height:50px; clear:both;">
 </div>
 
-
 <?php
-function txtclass($id, $h, $s) {
-	$a =explode("\n", $s);
-	echo '<div class="txt" id='.$id.'>';
-	echo '<h4 class="tag"><a name="'.$id.'">'.$title.'</a></h4>';
+/*
+function txtclass($name, $title, $a) {
+	//$a =explode("\n", $s);
+	echo '<div class="txt">';
+	echo '<h4 class="tag"><a name="'.$name.'">'.$title.'</a></h4>';
 	foreach ($a as $n => $p) {
 		echo " <p class=\"normal ${n}\" >　${p}</p>\n";
 	}
 	//echo "<p><a href=\"#\">目次</a></p>\n";
 	echo "</div>\n";
 }
-/*
+
 $data =file_get_contents('./xxxtmpdata/data.txt');
 $adata =explode("\n", $data);
 $hdata =explode(":", $adata[0]);
 $a =array_slice($adata, 1);
 txtclass($hdata[0], $hdata[1], $a);
 */
-
 ?>
+<a name="bot"></a>
+<br/><br/>
 <script>
+myNowloadingFini(2);
+
+<?php echo "const mobile =" .($MOBILE ? "1" : "0").";"; ?>
+console.log("mobile=%d", mobile);
+//xxx db.
+function xxximgtest() {
+	if (! mobile) {
+		let canvas =document.createElement("canvas");
+		let img =document.createElement("img");
+		img.setAttribute("src", "xxxtmpdata/m.svg");
+		canvas.width =543;
+		canvas.height =597;
+		canvas.getContext("2d").drawImage(img,0,0);
+		var b64 =canvas.toDataURL();
+		let imgtest =document.querySelector("#imgtest");
+		console.log("on pc, imgtest");
+		console.log(imgtest);
+		if (imgtest)
+			imgtest.onclick =function(e) {myOpenPhoto(e,b64,500,500);};
+	}
+}
+
+/*
+ *
+ * TxtClass.
+ *
+ */
 function txtclass(id, h, s) {
 	a =s.split("\n");
 	let txt ="";
 	let div =document.createElement("div"); 
-	div.id =id;
+	div.id ="txt"+id;
 	div.setAttribute("class", "txt");
-	div.setAttribute("id", id);
 	let h4 =document.createElement("h4"); 
 	h4.setAttribute("class", "tag");
 	let aName =document.createElement("a"); 
+	aName.id ="txt-aname"+id;
 	aName.setAttribute("name", id);
 	aName.innerHTML =h;
     h4.appendChild(aName);
     div.appendChild(h4);
-	console.info(a);
-	for (i =0; i<a.length; ++i ) {
+	console.info(aName);
+	//console.info(a);
+	for (let i =0; i<a.length; ++i ) {
 		let p =document.createElement("p"); 
+		p.id ="txt-p"+id+"-"+i;
 		p.setAttribute("class", "normal");
 		p.innerHTML =a[i];
     	div.appendChild(p);
 	}
     document.body.appendChild(div);
+	hodieALength =a.length;
+}
 
-//XXX db.
-	let canvas =document.createElement("canvas");
-	let img =document.createElement("img");
-	img.setAttribute("src", "xxxtmpdata/m.svg");
-	canvas.width =543;
-	canvas.height =597;
-	canvas.getContext("2d").drawImage(img,0,0);
-	var b64 =canvas.toDataURL();
-	document.querySelector("#imgtest").onclick =function(e) {myOpenPhoto(e,b64,500,500);};
+var hodieId =-1;
+var hodieActitatum =-1;
+var hodieALength =-1;
+var actitatum =10000;
+function txtclassAct(id, h, s) {
+	a =s.split("\n");
+	//console.info("id :"+id);
+	//console.info("h :"+h);
+	//console.info("a :"+a.length);
+	let txt ="";
+	let div =document.getElementById("txt"+id); 
+	//console.info("div :"+div);
+	let aName =document.getElementById("txt-aname"+id); 
+	if (aName.innerHTML != h)
+		aName.innerHTML =h;
+	//console.info(aName);
+	for (let i =0; i<a.length; ++i ) {
+		let p =document.getElementById("txt-p"+id+"-"+i); 
+		if (! p) {
+			let p =document.createElement("p"); 
+			p.id ="txt-p"+id+"-"+i;
+			p.setAttribute("class", "normal");
+			p.innerHTML =a[i];
+			div.appendChild(p);
+		} else if (p.innerHTML != a[i]) {
+			p.innerHTML =a[i];
+		}
+	}
+	let al =hodieALength;
+	hodieALength =a.length;
+	for (let i=a.length; i< al; ++i) {
+		let p =document.getElementById("txt-p"+id+"-"+i); 
+		div.removeChild(p);
+	}
 }
 function myRecvData(q, c, callback) {
 	console.log(q);
@@ -248,48 +285,86 @@ function myRecvData(q, c, callback) {
 	http.send(encodeURI(postdata.join("&")));
 }
 
+
 function mySetTxt(a,m,d) {
-	myRecvData(`SELECT * FROM luckxa_mydiary_txt WHERE a=${a} && m=${m} && ABS(d-${d}) < 2 ORDER BY a,m,d`, {"user" : "luckxa", "pass" : "MyLinka", "schema" : "l18"}, (data)=> {
+	myRecvData(`SELECT * FROM luckxa_mydiary_txt_test WHERE a=${a} && m=${m} && ABS(d-${d}) < 7 ORDER BY a,m,d`, {"user" : "luckxa", "pass" : "MyLinka", "schema" : "l18"}, (data)=> {
+	//myRecvData(`SELECT * FROM luckxa_mydiary_txt WHERE a=${a} && m=${m} && ABS(d-${d}) < 2 ORDER BY a,m,d`, {"user" : "luckxa", "pass" : "MyLinka", "schema" : "l18"}, (data)=> {
 			data =JSON.parse(data);
 			//console.log(data["error"]);
-			var res =data["res"];
-			for ( i =0; i<res.length; ++i) {
+			let res =data["res"];
+			//console.log("fetched ".(res.length));
+			console.log("fetched ");
+			console.log(res.length);
+			let aid =[];
+			for (let i =0; i<res.length; ++i) {
 				txtclass(res[i]["id"], res[i]["h"], res[i]["txt"]);
+				console.log("%d)id=%s,h=%s",i,res[i]["id"], res[i]["h"]);
+				aid.push(res[i]["id"]);
+			}
+			hodieId =aid[aid.length-1];
+			hodieActitatum =(new Date()).getTime();
+	});
+}
+(function initSetTxt() {
+	let dt =new Date();
+	let a =dt.getFullYear();
+	let m =dt.getMonth()+1;
+	let d =dt.getDate();
+	console.log("amd= %d,%d,%d",a,m,d);
+	mySetTxt(a, m, d);
+})();
+/*
+*/
+function myActTxt(id) {
+	myRecvData(`SELECT h,txt FROM luckxa_mydiary_txt_test WHERE id=${id}`, {"user" : "luckxa", "pass" : "MyLinka", "schema" : "l18"}, (data)=> {
+			data =JSON.parse(data);
+			if (data["error"])
+				console.log(data["error"]);
+			let res =data["res"];
+			if (res.length == 1) {
+				txtclassAct(id, res[0]["h"], res[0]["txt"]);
+			} else {
+				console.log("myActTxt fetched ");
+				console.log(res.length);
 			}
 	});
 }
-mySetTxt(2019, 4, 19);
-/*
-*/
-</script>
-<a name="bot"></a>
-<br/><br/>
-<script>
-my_nowloading_fini(2);
+document.body.onscroll =(e) => {
+	let body =document.body;
+	//console.log("Height"+(body.offsetHeight-body.clientHeight*1.2))
+	if ( body.scrollTop >= (body.offsetHeight-body.clientHeight*1.2)) {
+		if (hodieId > 0 && hodieActitatum < (new Date()).getTime() - actitatum) {
+			hodieActitatum = (new Date()).getTime();
+			//console.log("hodie id "+hodieId);
+			myActTxt(hodieId);
+		}
+	}
+}
 
-<?php
-if ($MOBILE) {
-	echo 'document.querySelector("#mypagetitle").onclick = () => {';
-} else {
-	echo 'document.querySelector("#mypagetitle").onmouseover = () => {';
-}
-?>
-//document.querySelector("#mypagetitle").onmouseover = () => {
-	document.querySelector(".titleicon").style.opacity = 1;
-	document.querySelector("h1").style.opacity = 0.1;
-	document.querySelector("#jumpsvg").style.height ="80px";
-	document.querySelector("#jumpsvg").style.opacity = 1;
-}
+/*
+ *
+ * Visual.
+ *
+ */
 document.querySelector("#mypagetitle").onmouseout = () => {
 	document.querySelector(".titleicon").style.opacity = 0.39;
 	document.querySelector("h1").style.opacity = 1;
 	document.querySelector("#jumpsvg").style.height ="10px";
 	document.querySelector("#jumpsvg").style.opacity = 0;
 }
+<?php
+if ($MOBILE) {
+	echo 'document.querySelector("#mypagetitle").onclick = () => {';
+} else {
+	echo 'document.querySelector("#mypagetitle").onmouseover = () => {';
+}
+		echo "document.querySelector('.titleicon').style.opacity = 1;";
+		echo "document.querySelector('h1').style.opacity = 0.1;";
+		echo "document.querySelector('#jumpsvg').style.height ='80px';";
+		echo "document.querySelector('#jumpsvg').style.opacity = 1;";
+	echo "}";
+?>
 
-
-/* Get the documentElement (<html>) to display the page in fullscreen */
-var elem = document.documentElement;
 
 /* View in fullscreen */
 function openFullscreen() {
@@ -306,22 +381,10 @@ function closeFullscreen() {
   }
 }
 
-function myMenuOver() {
-	document.getElementById("menu").style.opacity = 0.9;
-	//alert("my");
-}
-function myMenuOut() {
-	document.getElementById("menu").style.opacity = 0.1;
-	//alert("my");
-}
-
 function myClosePhoto(e) {
 	let tapis =document.getElementById("photo-tapis");
 	let img =document.getElementById("photo-img");
 	let info =document.getElementById("photo-info");
-	console.info("tapis "+tapis);
-	console.info("img "+img);
-	console.info("info "+info);
 	if (tapis.style.opacity != 0) {
 		info.style.transitionDuration ="1.5s";
 		info.style.backgroundColor ="black";
@@ -336,7 +399,7 @@ function myClosePhoto(e) {
 	}
 }
 function myOpenPhoto(e, data, width, height) {
-	var opacity =0.9;
+	var opacity =0.95;
 
 	let tapis =document.createElement("div"); tapis.id ="photo-tapis";
     document.body.insertBefore(tapis, document.body.firstChild);
@@ -361,16 +424,9 @@ function myOpenPhoto(e, data, width, height) {
 		openFullscreen();
 		let totalHeight =screen.availHeight;
 		let totalWidth =window.innerWidth;
-		//tapis.style.position ="fixed";
-		//info.style.position ="fixed";
-		//img.style.position ="fixed";
-		//tapis.style.margin ="0px";
-		//info.style.margin ="0px";
-		//img.style.margin ="0px";
 		tapis.style.backgroundImage ="linear-gradient(black, #113, #333, #311, black)";
 		tapis.style.opacity =info.style.opacity =opacity;
 		tapis.style.zIndex =img.style.zIndex =info.style.zIndex =5;
-		//img.style.zIndex =6;
 		let top =(totalHeight-height)/2-30;
 		let left =(totalWidth-width)/2-30;
 		info.style.top =top+"px";
@@ -385,19 +441,28 @@ function myOpenPhoto(e, data, width, height) {
 		img.setAttribute("src", "xxxtmpdata/m.svg");
 		img.setAttribute("class", "photoEnter");
 		photoHref.setAttribute("href", "xxxtmpdata/m.svg");
-		photoHref.setAttribute("download", "xxxtmpdata/m.svg");
+		photoHref.setAttribute("target", "_blank");
+		//photoHref.setAttribute("download", "xxxtmpdata/m.svg");
+		let imglinkicon ='<img class="data_image" width="12" height="12" alt="star" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAA/klEQVQoU3WQMZIBYRCFX48Jtgi4gZHZmkhAihsQErnBrhvsDRxhCIwQJyA2gRGokuEGEmqr7GrdGPUPNRP98/73/a9fE4zv0w++LeBHpKyp6/mfUN+0ynN6vSiOlk6K//qiVy9AlwlhijFLBJzxMpc+nWdEWK3blc7jge0TcP2AX1MAHqjZHS46APVAyMUAjZaLncXsgXj6NBN5zNhLWj4G6I91QU0KfR3TdiFzOjcg5nsHeyKdQvE0bqV1pIh2/UVf5qsSyDE3o9vT8m+AmhQCY64J0UOq6zJ2zdIhlmCWN5NN/Qa8b+mumAmRh4qjoJYE/H7YoY4RS0gyJ+lXBviSDaxQWVkAAAAASUVORK5CYII=">';
+		photoHref.innerHTML ="元の画像"+imglinkicon;
 /*
-		photoHref.innerHTML ="元の画像"+imgLinker;
 		siteHref.setAttribute("href", "xxxxxxtmpdata/m.svg");
 		siteHref.setAttribute("target", "_blank");
-		siteHref.innerHTML ="元のサイト"+imgLinker;
+		siteHref.innerHTML ="元のサイト"+imglinkicon;
 */
-		//legend.innerHTML ="My MyMelody My Mymelody Melody My Melody My Melody My Melody My Melody My Melody";
-		//tooltip.innerHTML ="My Melody My Melody My mymelody Melody My Melody My Melody My Melody foo<br>My Melody My Melody My Melody My Melody My Melody My Melody";
 		legend.innerHTML ="my diary";
-		tooltip.innerHTML ="my流華日記";
+		tooltip.innerHTML ="my流華日記の<br>QRコード";
 		tooltip.style.zIndex =7;
-		setTimeout(function(){window.onresize =myClosePhoto;}, 1000);
+		legend.onmouseover = () => {
+			tooltip.style.visibility ="visible";
+			tooltip.style.opacity =0.8;
+		}
+		legend.onmouseout = () => {
+			tooltip.style.visibility ="hidden";
+			tooltip.style.opacity =0;
+		}
+
+		setTimeout(()=>window.onresize =myClosePhoto, 1000);
 	}
 }
 
