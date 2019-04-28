98a98a386d331360b3c250ee2a8c426d7fcc6043
Luckxa <satlin_luckxa@yahoo.com>
2019-04-29 05:46:20 +0900

    noise,spectrum

--- study/snd/src/cardnoise.c
+++ study/snd/src/cardnoise.c
@@ -1,14 +1,41 @@
 #include "wave.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
 //void mono_wave_dump_header(char *file_name);
 
 
-static void writenoise( char *file_left, char *file_right) {
+static STEREO_PCM spcml, spcmr;
+static void allocnoise( double secs ) {
+	spcml.fs =8000;
+	spcml.bits =16;
+	spcml.length =int(spcml.fs * secs);
+  	spcml.s1 = (double*)calloc(spcml.length, sizeof(double));
+  	spcml.s2 = (double*)calloc(spcml.length, sizeof(double));
+	spcmr.fs =8000;
+	spcmr.bits =16;
+	spcmr.length =int(spcml.fs * secs);
+  	spcmr.s1 = (double*)calloc(spcml.length, sizeof(double));
+  	spcmr.s2 = (double*)calloc(spcml.length, sizeof(double));
 	int n;
-	static STEREO_PCM spcml, spcmr;
+	for(n=0; n<spcml.length; ++n) {
+		spcml.s1[n] =0;
+		spcml.s2[n] =0;
+		spcmr.s1[n] =0;
+		spcmr.s2[n] =0;
+	}
+}
+static void freenoise() {
+	free(spcml.s1);
+	free(spcml.s2);
+	free(spcmr.s1);
+	free(spcmr.s2);
+}
+
+	
+static void cracklenoise( double secs, char *file_left, char *file_right) {
 	spcml.fs =8000;
 	spcml.bits =16;
 	spcml.length =1600;
@@ -20,7 +47,7 @@ static void writenoise( char *file_left, char *file_right) {
   	spcmr.s1 = (double*)calloc(spcml.length, sizeof(double));
   	spcmr.s2 = (double*)calloc(spcml.length, sizeof(double));
 
-
+	int n;
 	for(n=0; n<spcml.length; ++n) {
 		double noise =drand48();
 		double x =((double)(n-88)/44*M_PI);
@@ -43,17 +70,103 @@ static void writenoise( char *file_left, char *file_right) {
 	free(spcmr.s2);
 }
 
+static void whitenoise( double secs, char *file_left, char *file_right ) {
+	allocnoise(secs);
+	int n;
+	printf("length %ld\n", spcml.length);
+	for( double hz =20; hz < 20000; hz*=1.05 ) {
+		for(n=0; n<spcml.length; ++n) {
+			double h =(2*M_PI)*n*hz/spcml.fs;
+			spcml.s1[n] =cos(h)/10;
+			spcmr.s2[n] =cos(h)/10;
+			//printf("%ld/%ld=%lf\n", n,spcml.length,h);
+		}
+	}
+	stereo_wave_write(&spcml, file_left);	
+	stereo_wave_write(&spcmr, file_right);	
+	freenoise();
+}
+
+static void pinknoise( double secs, char *file_left, char *file_right ) {
+	allocnoise(secs);
+	int n;
+	printf("length %ld\n", spcml.length);
+	for( double hz =20; hz < 20000; hz*=1.05 ) {
+		for(n=0; n<spcml.length; ++n) {
+			double h =(2*M_PI)*n*hz/spcml.fs;
+			double d =log2(n/20.0);
+			spcml.s1[n] =cos(h)/d;
+			spcmr.s2[n] =cos(h)/d;
+			//printf("%ld/%ld=%lf\n", n,spcml.length,h);
+		}
+	}
+	stereo_wave_write(&spcml, file_left);	
+	stereo_wave_write(&spcmr, file_right);	
+	freenoise();
+}
+static void rednoise( double secs, char *file_left, char *file_right ) {
+	allocnoise(secs);
+	int n;
+	printf("length %ld\n", spcml.length);
+	for( double hz =20; hz < 20000; hz*=1.05 ) {
+		for(n=0; n<spcml.length; ++n) {
+			double h =(2*M_PI)*n*hz/spcml.fs;
+			double d =log2(n/20.0);
+			spcml.s1[n] =cos(h)/d;
+			spcmr.s2[n] =cos(h)/d;
+			//printf("%ld/%ld=%lf\n", n,spcml.length,h);
+		}
+	}
+	stereo_wave_write(&spcml, file_left);	
+	stereo_wave_write(&spcmr, file_right);	
+	freenoise();
+}
+static void clicknoise( double secs, char *file_left, char *file_right ) {
+	allocnoise(secs);
+	int n;
+	for(n=spcml.length/2; n<spcml.length/2+1; ++n) {
+		double h =0.5;
+		spcml.s1[n] =cos(h);
+		spcmr.s2[n] =cos(h);
+		printf("%ld/%ld=%lf\n", n,spcml.length,h);
+	}
+	stereo_wave_write(&spcml, file_left);	
+	stereo_wave_write(&spcmr, file_right);	
+	freenoise();
+}
+
+
+
 /*
  argv[1] = left,
  argv[2] = right
  */
 int main(int argc, char *argv[]) {
-	int i;
-	if (argc != 3) {
-		printf("usage : %s left.wav right.wav\n", argv[0]);
+	if (argc < 3) {
+		printf("usage : %s [cracklenoise|white|pink|red] secs [left.wav [right.wav]]\n", argv[0]);
 		exit(1);
 	}
-	writenoise(argv[1], argv[2]);
+	char *ssecs =argv[2];
+	char *left =NULL;
+	char *right =NULL;
+	double secs =atof(ssecs);
+	if (argc >= 4) {
+		left =argv[3];
+	}
+	if (argc >= 5) {
+		right =argv[4];
+	}
+	if (strcmp(argv[1], "cracklenoise") == 0) {
+		cracklenoise(secs, left, right);
+	} else if (strcmp(argv[1], "white") == 0) {
+		whitenoise(secs, left, right);
+	} else if (strcmp(argv[1], "pink") == 0) {
+		pinknoise(secs, left, right);
+	} else if (strcmp(argv[1], "red") == 0) {
+		rednoise(secs, left, right);
+	} else if (strcmp(argv[1], "click") == 0) {
+		clicknoise(secs, left, right);
+	}
 	return 0;
 }
 
--- study/snd/src/wave_dump.c
+++ study/snd/src/wave_dump.c
@@ -6,7 +6,7 @@
 static MONO_PCM pcm;
 static STEREO_PCM spcm;
 
-static void monopcm_stat( char *file_name) {
+static void monopcm_stat( double de, double ad, char *file_name) {
 	mono_wave_dump_header(file_name);
 	int n;
 	double amax =0, amin =0;
@@ -14,20 +14,48 @@ static void monopcm_stat( char *file_name) {
 
 	mono_wave_read(&pcm, file_name);
 
-	for(n=0; n<pcm.length; ++n) {
-		double a =pcm.s[n];
-		amax =fmax(a,amax);
-		amin =fmin(a,amin);
-		a0 +=a/pcm.length;
+	double spec[19980];
+	for( double hz =20; hz < 20000; hz+=1 ) {
+		spec[(int)hz-20] =0;
+	}
+	for( double hz =20; hz < 20000; hz+=1 ) {
+		for(n=(int)fmin(pcm.length, de*pcm.fs); n<(int)fmin(pcm.length, ad*pcm.fs); ++n) {
+			double a =pcm.s[n];
+			amax =fmax(a,amax);
+			amin =fmin(a,amin);
+			a0 +=a/pcm.length;
+			spec[(int)hz-20] +=a*sin(M_PI*n*hz/pcm.fs)/((ad-de)*pcm.length);
+		}
 	}
 	printf("max:%f, min:%f, a0:%lf\n", amax, amin, a0);
 	free(pcm.s);
+	amax =0; amin =0;
+	for( double hz =20; hz < 20000; hz+=1 ) {
+		amax =fmax(amax,spec[(int)hz-20]);
+		amin =fmin(amin,spec[(int)hz-20]);
+	}
+	FILE *gp =popen("gnuplot -persist", "w");
+	fprintf(gp, "set xrange [20:20000]\n");
+	fprintf(gp, "set yrange [%f:%f1]\n", amin, amax);
+	fprintf(gp, "plot '-' with lines\n");
+	for( int hz =20; hz < 20000; hz+=1 ) {
+		printf("%d %f\n",hz,spec[hz-20]);
+		fprintf(gp, "%d %f\n",hz,spec[hz-20]);
+	}
+	fprintf(gp, "e\n");
 }
 
+
 int main(int argc, char *argv[]) {
+	if (argc < 4) {
+		printf("Usage : %s de(sec) ad(sec) [files...]\n", argv[1]);
+		exit(1);
+	}
+	double de =atof(argv[1]);
+	double ad =atof(argv[2]);
 	int i;
-	for(i=1; i<argc; ++i) {
-		monopcm_stat(argv[i]);
+	for(i=3; i<argc; ++i) {
+		monopcm_stat(de, ad, argv[i]);
 	}
 	return 0;
 }
