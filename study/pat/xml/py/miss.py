28a3134fc0faf405caba9a6bee3919718e59fdaf
Luckxa <satlin_luckxa@yahoo.com>
2019-05-31 19:25:51 +0900

    ...

--- study/pat/xml/py/miss.py
+++ study/pat/xml/py/miss.py
@@ -26,11 +26,17 @@ class textmissex :
 
 class calcmissex :
 
+  def style(ipse, data) :
+    for d in data :
+      if d["name"].split(":")[1] == "style-name" :
+        return ipse.sdefs.style[d["value"]]
+    return {}
+
   def __init__(ipse) :
     ipse.lodoc = legodoc(debug=1)
     ipse.lodoc.on("office:body", "closetag", ipse.closebody)
     ipse.lodoc.on("office:body", "opentag", ipse.openbody)
-    ipse.styl =odocstyledefs(ipse.lodoc)
+    ipse.sdefs =odocstyledefs(ipse.lodoc)
 
   def closetablerow (ipse, ev, data) : 
     ipse.t.append(ipse.cell)
@@ -51,17 +57,33 @@ class calcmissex :
 
   def closetablecell (ipse, ev, data) : 
     ipse.lodoc.no("fragment", ipse.cellfrag)
+    ipse.lodoc.no("text:span", "opentag", ipse.openspan)
+    ipse.lodoc.no("table:table-cell", "closetag", ipse.closetablecell)
     ipse.cell.append(ipse.frag)
     del ipse.frag
 
-  def opentablecell (ipse, ev, data) : 
+  def openspan (ipse, ev, data) : 
+    s =ipse.style(data)
+    if "fo:font-style" in s and s["fo:font-style"] == "italic" :
+      ipse.lodoc.on("text:span", "closetag", ipse.closespan)
+      ipse.lodoc.no("fragment", ipse.cellfrag)
+  
+  def closespan (ipse, ev, data) : 
+    ipse.lodoc.no("text:span", "closetag", ipse.closespan)
     ipse.lodoc.on("fragment", ipse.cellfrag)
-    ipse.frag =""
+    pass
+
+  def opentablecell (ipse, ev, data) : 
+    s =ipse.style(data)
+    if "fo:font-style" not in s or s["fo:font-style"] != "italic" :
+      ipse.lodoc.on("fragment", ipse.cellfrag)
+      ipse.lodoc.on("text:span", "opentag", ipse.openspan)
+      ipse.lodoc.on("table:table-cell", "closetag", ipse.closetablecell)
+      ipse.frag =""
 
   def openbody (ipse, ev, data) : 
     ipse.lodoc.on("table:table", "opentag", ipse.opentable)
     ipse.lodoc.on("table:table-cell", "opentag", ipse.opentablecell)
-    ipse.lodoc.on("table:table-cell", "closetag", ipse.closetablecell)
     ipse.lodoc.on("table:table-row", "closetag", ipse.closetablerow)
     ipse.t =None
     ipse.tt ={}
@@ -75,16 +97,22 @@ class calcmissex :
 
   def __call__(ipse, fnom) :
     ipse.lodoc(fnom)
+    return ipse.tt
 
 if __name__ == "__main__" :
   from sys import argv
   if len(argv) < 3 :
-    print("modus : miss de ad")
+    print("modus : miss *.ods */   italicis-commentatae csv.")
   print("%s -> %s"%(argv[1], argv[2]))
-  cm =calcmissex()
-  for x in cm(argv[1]) :
-    print(x)
-  exit()
+  de,ad =argv[1:]
+  if de[-3:].lower() == "ods" :
+    cm =calcmissex()
+    for snom,data in cm(argv[1]).items() :
+      of =open(ad+"/"+snom.replace(" ", "-")+".sql", "w")
+      for l in data :
+        if l :
+          of.write(" ".join(l) + ";\n")
+    exit()
   def odtread(file_name) :
     se =textmissex()
     se(file_name)
